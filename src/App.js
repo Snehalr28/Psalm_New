eb6da91775dc8f0b6ae49266d2cef3e5160ce6dd
SnehalClarion <snehal.ramteke@clariontech.com>
2023-02-17 07:52:12 +0530

    Added new branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SplashScreen from 'react-native-splash-screen'; //import SplashScreen
 
 function App() {
   useEffect(() => {
-    SplashScreen.hide(); //hides the splash screen on app load.
+    SplashScreen.hide(); //hides the splash screen on app load...
   }, []);
   return (
     <Provider store={store}>
